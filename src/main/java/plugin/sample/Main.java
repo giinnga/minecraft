f3b79a52b0f0c7816473c8e0ba12023a37a37695
ginga <izuto.kudo.cop@gmail.com>
2023-09-01 23:57:41 +0900

    tamaya変更

--- src/main/java/plugin/sample/Main.java
+++ src/main/java/plugin/sample/Main.java
@@ -75,7 +75,7 @@ public final class Main extends JavaPlugin implements Listener {
                 firework.setFireworkMeta(fireworkMeta);
             }
                 Path path = Path.of("firework.txt");
-                Files.writeString(path, "たーまやー");
+                Files.writeString(path, "tamaya");
                 player.sendMessage(Files.readString(path));
         }
         count++;
