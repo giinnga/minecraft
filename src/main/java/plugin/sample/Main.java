51458e0fc8d5aa34786f1dc7191062a2e32608d3
ginga <izuto.kudo.cop@gmail.com>
2023-09-09 13:55:13 +0900

    花火カウント機能追加

--- src/main/java/plugin/sample/Main.java
+++ src/main/java/plugin/sample/Main.java
@@ -75,11 +75,12 @@ public final class Main extends JavaPlugin implements Listener {
                 // 追加した情報で再設定する。
                 firework.setFireworkMeta(fireworkMeta);
             }
-              // Path path = Path.of("firework.txt");
-              // Files.writeString(path, "tamaya");
-              // player.sendMessage(Files.readString(path));
+               Path path = Path.of("firework.txt");
+               Files.writeString(path, "tamaya");
+               player.sendMessage(Files.readString(path));
         }
         count++;
+        System.out.println("今のカウント値" + count);
     }
 
     @EventHandler
